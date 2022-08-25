a2f1380cc16a1ff253a608332cd8bc8794824858
Jens Peter Lillebæk <jenspeter@lillebak.dk>
2022-08-25 17:57:23 +0200

    CourseModule 6.Migration - Added sqlite.

---
+++ CityInfo_ASP.NETCore6WebApiFundamentalsCourseDemo/CityInfo/DbContexts/CityInfoContext.cs
@@ -0,0 +1,15 @@
+﻿using CityInfo.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.DbContexts
+{
+    public class CityInfoContext : DbContext
+    {
+        public DbSet<City> Cities { get; set; } = null!;
+        public DbSet<PointOfInterest> PointsOfInterest { get; set; } = null!;
+
+        public CityInfoContext(DbContextOptions<CityInfoContext> options) : base(options)
+        {
+        }
+    }
+}
--- CityInfo_ASP.NETCore6WebApiFundamentalsCourseDemo/CityInfo/Program.cs
+++ CityInfo_ASP.NETCore6WebApiFundamentalsCourseDemo/CityInfo/Program.cs
@@ -1,6 +1,8 @@
 using CityInfo;
+using CityInfo.DbContexts;
 using CityInfo.Services;
 using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 Log.Logger = new LoggerConfiguration()
@@ -37,6 +39,9 @@ builder.Services.AddTransient<IMailService, CloudMailService>();
 #endif
 
 builder.Services.AddSingleton<CitiesDataStore>();
+
+builder.Services.AddDbContext<CityInfoContext>(
+    dbContextOptions => dbContextOptions.UseSqlite("Data Source=CityInfo.db"));
 
 var app = builder.Build();
 
